6178208f19be044ab00c03edc3bb86088bbbce0e
Ivan Milev <ivanmilevTUES@gmail.com>
2017-11-05 21:43:52 +0200

    Fix on major buf on the data splitting fucntion

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 from sklearn.metrics import average_precision_score
 import matplotlib.pyplot as plt
+from preprocessing_utilities import get_unused_dataset_indxs, get_unused_features
 
 
 def split_to_train_test(features_and_labels: list, test_set_percent=0.4, shuffle=True) -> tuple:
@@ -14,8 +15,8 @@ def split_to_train_test(features_and_labels: list, test_set_percent=0.4, shuffle
         np.random.shuffle(features_and_labels)
     features = features_and_labels[:, :-1]
     labels = features_and_labels[:, -1:].ravel()
-    return (features[:int(len(features) * test_set_percent)], features[:-int(len(features) * test_set_percent)],
-            labels[:int(len(labels) * test_set_percent)], labels[:-int(len(labels) * test_set_percent)])
+    return (features[:int(len(features) * test_set_percent)], features[-int(len(features) * (1.-test_set_percent)):],
+            labels[:int(len(labels) * test_set_percent)], labels[-int(len(labels) * (1. - test_set_percent)):])
 
 
 def log_classifier(clf, test_set_acc, train_set_acc, time_start, time_end, precision):
@@ -35,6 +36,7 @@ Precision: {}
 
 
 def train_and_log(clf_class, features_train, labels_train, features_test, labels_test):
+    print(clf_class.__name__ + " has started training.")
     clf = clf_class()
     time_start = time.time()
     clf.fit(features_train, labels_train.T)
@@ -46,7 +48,7 @@ def train_and_log(clf_class, features_train, labels_train, features_test, labels
                    time_start, time_end, precision)
 
 
-def train_classifiers(features_train, labels_train, features_test, labels_test):
+def train_classifiers(features_test, features_train, labels_test, labels_train):
     ## Logistic regresion
     from sklearn.linear_model import LogisticRegression
     train_and_log(LogisticRegression, features_train, labels_train, features_test, labels_test)
@@ -55,8 +57,8 @@ def train_classifiers(features_train, labels_train, features_test, labels_test):
     train_and_log(GaussianNB, features_train, labels_train, features_test, labels_test)
 
     ### SVM
-    from sklearn.svm import SVC
-    train_and_log(SVC, features_train, labels_train, features_test, labels_test)
+    # from sklearn.svm import SVC
+    # train_and_log(SVC, features_train, labels_train, features_test, labels_test)
 
     ### DecisionTree
     from sklearn import tree
@@ -65,6 +67,31 @@ def train_classifiers(features_train, labels_train, features_test, labels_test):
     ### RandomForest
     from sklearn.ensemble import RandomForestClassifier
     train_and_log(RandomForestClassifier, features_train, labels_train, features_test, labels_test)
+
+
+def plot(data):
+    x = list(range(len(data)))
+    y = data
+
+    plt.plot(x, y, 'ro')
+    plt.axis([0, len(x), min(y), max(y)])
+
+    plt.xlabel('feature number')
+    plt.ylabel('feature importance')
+    plt.title('Features importance rate')
+
+    plt.show()
+
+
+def terminal_testing(clf, features):
+    lp = LineParser(features)
+
+    for _ in range(10):
+        inp = input("Your hate here:")
+        ds = lp.parse_line(inp)
+        print(clf.predict_proba(ds))
+        print(clf.predict(ds))
+        print('-----------')
 
 
 def main():
@@ -83,25 +110,25 @@ def main():
 
     full_dataset = np.concatenate((labeled_data, unlabeled_data), axis=0)
 
-    # dataset = PreprocessData.reduce_dataset(full_dataset)
-    # features = PreprocessData.reduce_features(full_dataset, features)
+    features_test, features_train, labels_test, labels_train = split_to_train_test(full_dataset, test_set_percent=0.2, shuffle=True)
 
-    features_test, features_train, labels_test, labels_train = split_to_train_test(full_dataset, test_set_percent=0.4, shuffle=True)
-
-    # print(features)
+    print(features_test.shape, features_train.shape, labels_train.shape, labels_test.shape)
+    train_classifiers(features_test, features_train, labels_test, labels_train)
 
     from sklearn.ensemble import RandomForestClassifier
     clf = RandomForestClassifier()
     clf.fit(features_train, labels_train)
 
-    lp = LineParser(features)
+    indx_to_delete = get_unused_features(clf.feature_importances_)
+    dataset = PreprocessData.reduce_dataset(full_dataset, indx_to_delete)
+    features_test, features_train, labels_test, labels_train = split_to_train_test(dataset, test_set_percent=0.2, shuffle=True)
 
-    for _ in range(10):
-        inp = input("Your hate here:")
-        ds = lp.parse_line(inp)
-        print(clf.predict_proba(ds))
-        print(clf.predict(ds))
-        print('-----------')
+    print(features_test.shape, features_train.shape, labels_train.shape, labels_test.shape)
+    train_classifiers(features_test, features_train, labels_test, labels_train)
+
+    # plot(clf.feature_importances_ )
+
+
 
 if __name__ == "__main__":
     main()
