4a2b72b8c38fad6037cda9dbdcfc7d4e0b3ac325
Ivan Milev <ivanmilevTUES@gmail.com>
2017-12-11 10:23:14 +0200

    Refactoring the model.py file. Added QuestionModel and first results

--- question_model.py
+++ question_model.py
@@ -1,5 +1,6 @@
 from preprocess.PreprocessQuestions import PreprocessQuestions
-
+from preprocess.PreprocessData import PreprocessData
+from preprocess.preprocessing_utilities import split_to_train_test, train_classifiers
 
 def init_features():
     pq = PreprocessQuestions(['questions'], ['question_types01.txt'])
@@ -7,12 +8,13 @@ def init_features():
     pq.save_features('data/processed_data/questions_full_features.pickle')
 
 def main():
-    pq = PreprocessQuestions(['questions'], ['question_types01.txt'])
-    pq.load_features('data/processed_data/questions_full_features.pickle')
-    pq.init_dataset()
-    pq.save_dataset('data/processed_data/dataset_questions_full_features.pickle')
-    print(pq.labels)
-    print(len(pq.labels))
+    pd = PreprocessData([''], [''])
+    ds = pd.load_and_get_dataset('data/processed_data/dataset_questions_full_features.pickle')
+    features_test, features_train, labels_test, labels_train = split_to_train_test(ds, test_set_percent=0.2,
+                                                                                   shuffle=True)
+    train_classifiers(features_test, features_train, labels_test, labels_train)
+    
+
 
 if __name__ == '__main__':
     main()
