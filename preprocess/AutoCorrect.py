97f19a0a2c88290755dc7b8f9b06a7de85c04c14
Ivan Milev <ivanmilevTUES@gmail.com>
2017-12-18 19:26:13 +0200

    changed saving extention to .pkl

--- preprocess/AutoCorrect.py
+++ preprocess/AutoCorrect.py
@@ -26,15 +26,15 @@ class AutoCorrect(FileOpenerMixin):
         self.close_files(files)
 
     @not_none('slang_dict')
-    def save_slang_dict(self, file='slang_correction.pickle'):
+    def save_slang_dict(self, file='slang_correction.pkl'):
         with open(file, 'wb') as f:
             pickle.dump(self.slang_dict, f)
 
-    def load_slang_dict(self, file='slang_correction.pickle'):
+    def load_slang_dict(self, file='slang_correction.pkl'):
         with open(file, 'rb') as f:
             self.slang_dict = pickle.load(f)
     
-    def load_and_get_slang_dict(self,  file='slang_correction.pickle'):
+    def load_and_get_slang_dict(self,  file='slang_correction.pkl'):
         with open(file, 'rb') as f:
             self.slang_dict = pickle.load(f)
         return self.slang_dict
@@ -57,15 +57,15 @@ class AutoCorrect(FileOpenerMixin):
         self.spell_correct_dict = spell_correct_dict
 
     @not_none('spell_correct_dict')
-    def save_spell_correction(self, file="spell_correct.pickle"):
+    def save_spell_correction(self, file="spell_correct.pkl"):
         with open(file, 'wb') as f:
             pickle.dump(self.spell_correct_dict, f)
 
-    def load_spell_correct(self, file="spell_correct.pickle"):
+    def load_spell_correct(self, file="spell_correct.pkl"):
         with open(file, 'rb') as f:
             self.spell_correct_dict = pickle.load(f)
 
-    def load_and_get_spell_correct(self, file="spell_correct.pickle"):
+    def load_and_get_spell_correct(self, file="spell_correct.pkl"):
         with open(file, 'rb') as f:
             self.spell_correct_dict = pickle.load(f)
         return self.spell_correct_dict
