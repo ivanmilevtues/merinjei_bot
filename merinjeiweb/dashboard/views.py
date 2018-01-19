4c576cac14e8b88070861f8cf2dea9fd4354be18
ivanmilevtues <ivanmilevtues@gmail.com>
2018-01-19 12:27:42 +0200

    Added pages list in the dashboard

--- merinjeiweb/dashboard/views.py
+++ merinjeiweb/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from allauth.socialaccount.models import SocialToken, SocialApp
 import requests
+from pprint import pprint
 import json
 
 
@@ -9,13 +10,21 @@ def profile_handler(request):
     access_token = SocialToken.objects.get(
         account__user=user, account__provider='facebook')
     access_token = access_token.token
-    response = requests.get('https://graph.facebook.com/v2.11/me?access_token=' + access_token)
-    data = json.loads(response._content)
-    user = data['name']
 
-    # This code will be enabled after facebook review for the application
-    # user_id =  data['id']
-    # response = requests.get('https://graph.facebook.com/v2.11/' + user_id + '/accounts?access_token=' + access_token)
-    # pages_with_perms = json.loads(response._content)['data']
+    user_response = requests.get(
+        'https://graph.facebook.com/v2.11/me?fields=picture,name&access_token=' + access_token)
+
+    fb_pages_response = requests.get(
+        'https://graph.facebook.com/v2.11/me/accounts?type=page&access_token=' + access_token)
+    
+    user_data = json.loads(user_response._content)
+    fb_page_data = json.loads(fb_pages_response._content)
+    username = user_data['name']
+    user_pic = user_data['picture']['data']['url']
+
+    fb_pages = []
+
+    for page in fb_page_data['data']:
+        fb_pages.append(page['name'])
 
     return render(request, 'profile.html', locals())
